947d7759bea48596a0a4d63f323fab0eb4b97d5c
Ceyda Sevilmis <ceyda.sevilmis@outlook.com>
2023-04-13 21:04:06 +0300

    Add item scriptable object
    - Item script
    - Five food items

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Object/Item")]
+public class Item : ScriptableObject
+{
+    public string id;
+    public ItemType type;
+    public Sprite image;
+    public GameObject prefab;
+    public bool stackable = true;
+}
+
+public enum ItemType
+{
+    Food
+}
