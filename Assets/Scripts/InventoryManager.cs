4aef63081974dd4d646aca2248cc5e9bcfd46d7e
Ceyda Sevilmis <ceyda.sevilmis@outlook.com>
2023-04-19 18:20:22 +0300

    Add inventory count text

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     public InventorySlot[] inventorySlots;
     public GameObject inventoryItemPrefab;
+
+    public Text inventoryCountText;
+    private int occupiedSlots;
+    private int inventoryCapacity;
+
+    private void Start()
+    {
+        InitializeInventoryCount();
+        RefreshInventoryCount();
+    }
 
     public bool AddItem(Item item)
     {
@@ -29,5 +40,26 @@ public class InventoryManager : MonoBehaviour
         GameObject newItemGO = Instantiate(inventoryItemPrefab, slot.transform);
         InventoryItem inventoryItem = newItemGO.GetComponent<InventoryItem>();
         inventoryItem.InitialiseItem(item);
+    }
+
+    void InitializeInventoryCount()
+    {
+        int count = 0;
+
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            if (inventorySlots[i].GetComponentInChildren<InventoryItem>() != null)
+            {
+                count++;
+            }
+        }
+
+        occupiedSlots = count;
+        inventoryCapacity = inventorySlots.Length;
+    }
+
+    void RefreshInventoryCount()
+    {
+        inventoryCountText.text = $"{occupiedSlots} / {inventoryCapacity}";
     }
 }
